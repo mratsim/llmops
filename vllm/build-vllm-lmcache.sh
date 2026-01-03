dd17089f05789583267afb4c50f5d5ba85e0f1fc
Mamy Ratsimbazafy <mamy_github@numforge.co>
2026-01-04 00:16:12 +0100

    Address review comments

--- vllm/build-vllm-lmcache.sh
+++ vllm/build-vllm-lmcache.sh
@@ -7,17 +7,18 @@ set -euo pipefail
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$SCRIPT_DIR"
 
+IMAGE_TAG=vllm-202601-cu129
 UVCACHE_DIR=$SCRIPT_DIR/"vllm-uvcache"
 CCACHE_DIR=$SCRIPT_DIR/"vllm-ccache"
 TMPDIR=$SCRIPT_DIR/"vllm-dockercache"
 
-mkdir -p "$CCACHE_DIR"
-mkdir -p "$TMPDIR"
+mkdir -p "${UVCACHE_DIR}"
+mkdir -p "${CCACHE_DIR}"
+mkdir -p "${TMPDIR}"
 
-TMPDIR="$TMPDIR" podman build \
-    -v "$UVCACHE_DIR":/root/.cache/uv \
-    -v "$CCACHE_DIR:/root/.ccache" \
-    -t vllm-202601-cu129 \
+TMPDIR="${TMPDIR}" podman build \
+    -v "${UVCACHE_DIR}":/root/.cache/uv \
+    -v "${CCACHE_DIR}":/root/.ccache \
+    -t ${IMAGE_TAG} \
     -f vllm-lmcache-Dockerfile \
     "$@"
-ccache -s
